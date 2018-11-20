31a53348b8414a699f20cb259e5ce580a91c2caf
Kevin Wilson <khwilson@gmail.com>
2018-11-20 00:02:29 -0500

    Add docstrings to convert.py

--- convert.py
+++ convert.py
@@ -1,3 +1,11 @@
+"""
+Convert Sentencing Commission files into CSVs.
+
+Usage:
+  $ python3 convert.py [LIST OF FILES TO CONVERT]
+
+@author Kevin H. Wilson <khwilson@gmail.com>
+"""
 import csv
 import os
 import sys
@@ -8,17 +16,35 @@ import click
 
 
 def read_columns(filename):
+  """
+  Read the column names from the file with the passed name.
+  Assumes it's in a SAS format that begins with INPUT and ends with
+  a semicolon.
+  
+  Args:
+    filename (str): The file name to read
+    
+  Returns:
+    list[dict]: The list of column names where each element is a dict with key/vals:
+      * name (str): The column name
+      * is_char (bool): Whether the column is a string column
+      * start (int): The (1-delimited) starting position of the column
+      * end (int): The (1-delimited and inclusive) ending position of the column
+  """
   columns = []
-  with open(filename) as f:
+  with open(filename, 'rt') as f:
+    # Search for the line that starts with INPUT
     for line in f:
       if line.startswith('INPUT'):
         break
-    
+
     for line in f:
+      # Kill all the extra whitespace
       line = line.strip()
     
       # Is this the last line?
       if line.endswith(';'):
+        # If so, strip the ; and the extra whitespace
         last_line = True
         line = line[:-1].strip()
       else:
@@ -30,57 +56,91 @@ def read_columns(filename):
       while i < len(sline):
         col_name = sline[i]
         i += 1
+
         if sline[i] == '$':
           is_char = True
           i += 1
         else:
           is_char = False
+
         field_range = sline[i]
         i += 1
+
+        # Field ranges are formatted either as # or #-#
         sfield_range = field_range.split('-')
         if len(sfield_range) == 1:
           sfield_range = (sfield_range[0], sfield_range[0])
+
+        # Write out the column to the list
         columns.append({
           'name': col_name,
           'is_char': is_char,
           'start': int(sfield_range[0]),
           'end': int(sfield_range[1])
         })
+
       if last_line:
         break
+
   return columns
 
 
 def convert_file(filename):
+  """
+  Convert a file from the Sentencing Commission format into a CSV.
+  Assumes the file is a ZIP file containing at least the following:
+    - .sas: A file with the same name as `filename` except ending in .sas
+    - .dat: A file with the same name as `filename` except ending in .dat
+    
+  The .dat file is a fixed-width file whose columns are described by the .sas
+  file. If you're looking at the .sas file, search for INPUT and LENGTH to
+  see the two main parts of the file. There are a _lot_ of columns.
+  
+  Args:
+    filename(str): The name of the file to convert
+  """
   with tempfile.TemporaryDirectory() as tmpdir:
+    # Unzip the contents of the file
     with zipfile.ZipFile(filename, 'r') as thefile:
       thefile.extractall(tmpdir)
 
+    # Read in the column names from the .sas file
     sasfilename = os.path.basename(filename)[:-4] + '.sas'
     saspath = os.path.join(tmpdir, sasfilename)
     columns = read_columns(saspath)
 
+    # Setup the path to the .dat file
     datfilename = os.path.basename(filename)[:-4] + '.dat'
     datpath = os.path.join(tmpdir, datfilename)
 
+    # Open the output file
     outfilename = filename[:-4] + '.csv'
     with open(outfilename, 'wt') as outfile:
+      # Write the column headers
       writer = csv.writer(outfile)
       writer.writerow([col['name'] for col in columns])
+      
+      # Read in the data
       with click.progressbar(length=os.stat(datpath).st_size) as bar:
         with open(datpath, 'rb') as infile:
           for line in infile:
             bar.update(len(line))
             line = line.decode('utf8')
+            
+            # Read in a single row
             readrow = []
             for col in columns:
               val = line[col['start'] - 1:col['end']].strip()
+              
+              # If it's numeric and not missing, format it nicely
               if val and not col['is_char']:
                 if '.' in val:
                   val = float(val)
                 else:
                   val = int(val)
               readrow.append(val)
+
+            # Write out the row
             writer.writerow(readrow)
 
 
