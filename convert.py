0bfacbecc4f815059c904bc9d5799bf7e76713a8
Kevin Wilson <khwilson@gmail.com>
2020-10-21 19:29:12 -0400

    Add 2018 and 2019 data
    Also black-ify the conversion script and add the full processing
    pipeline to the ./getdata.sh

--- convert.py
+++ convert.py
@@ -11,149 +11,157 @@ import os
 import sys
 import tempfile
 import zipfile
+from pathlib import Path
+from typing import Dict, List, Union
 
 import click
 
 
-def read_columns(filename):
-  """
-  Read the column names from the file with the passed name.
-  Assumes it's in a SAS format that begins with INPUT and ends with
-  a semicolon.
-  
-  Args:
-    filename (str): The file name to read
-    
-  Returns:
-    list[dict]: The list of column names where each element is a dict with key/vals:
-      * name (str): The column name
-      * is_char (bool): Whether the column is a string column
-      * start (int): The (1-delimited) starting position of the column
-      * end (int): The (1-delimited and inclusive) ending position of the column
-  """
-  columns = []
-  with open(filename, 'rt') as f:
-    # Search for the line that starts with INPUT
-    for line in f:
-      if line.startswith('INPUT'):
-        break
+def read_columns(filename: Union[str, Path]) -> List[Dict[str, Union[bool, int, str]]]:
+    """
+    Read the column names from the file with the passed name.
+    Assumes it's in a SAS format that begins with INPUT and ends with
+    a semicolon.
 
-    for line in f:
-      # Kill all the extra whitespace
-      line = line.strip()
-    
-      # Is this the last line?
-      if line.endswith(';'):
-        # If so, strip the ; and the extra whitespace
-        last_line = True
-        line = line[:-1].strip()
-      else:
-        last_line = False
-    
-      # Parse row into column names
-      i = 0
-      sline = line.split()
-      while i < len(sline):
-        col_name = sline[i]
-        i += 1
+    Args:
+      filename: The file name to read
 
-        if sline[i] == '$':
-          is_char = True
-          i += 1
-        else:
-          is_char = False
+    Returns:
+      The list of column names where each element is a dict with key/vals:
+        * name (str): The column name
+        * is_char (bool): Whether the column is a string column
+        * start (int): The (1-delimited) starting position of the column
+        * end (int): The (1-delimited and inclusive) ending position of the column
+    """
+    columns = []
+    with open(filename, "rt") as f:
+        # Search for the line that starts with INPUT
+        for line in f:
+            if line.startswith("INPUT"):
+                break
 
-        field_range = sline[i]
-        i += 1
+        for line in f:
+            # Kill all the extra whitespace
+            line = line.strip()
 
-        # Field ranges are formatted either as # or #-#
-        sfield_range = field_range.split('-')
-        if len(sfield_range) == 1:
-          sfield_range = (sfield_range[0], sfield_range[0])
+            # Is this the last line?
+            if line.endswith(";"):
+                # If so, strip the ; and the extra whitespace
+                last_line = True
+                line = line[:-1].strip()
+            else:
+                last_line = False
 
-        # Write out the column to the list
-        columns.append({
-          'name': col_name,
-          'is_char': is_char,
-          'start': int(sfield_range[0]),
-          'end': int(sfield_range[1])
-        })
+            # Parse row into column names
+            i = 0
+            sline = line.split()
+            while i < len(sline):
+                col_name = sline[i]
+                i += 1
 
-      if last_line:
-        break
-
-  return columns
-
-
-def convert_file(filename):
-  """
-  Convert a file from the Sentencing Commission format into a CSV.
-  Assumes the file is a ZIP file containing at least the following:
-    - .sas: A file with the same name as `filename` except ending in .sas
-    - .dat: A file with the same name as `filename` except ending in .dat
-    
-  The .dat file is a fixed-width file whose columns are described by the .sas
-  file. If you're looking at the .sas file, search for INPUT and LENGTH to
-  see the two main parts of the file. There are a _lot_ of columns.
-  
-  Args:
-    filename(str): The name of the file to convert
-  """
-  with tempfile.TemporaryDirectory() as tmpdir:
-    # Unzip the contents of the file
-    with zipfile.ZipFile(filename, 'r') as thefile:
-      thefile.extractall(tmpdir)
-
-    # Read in the column names from the .sas file
-    sasfilename = os.path.basename(filename)[:-4] + '.sas'
-    saspath = os.path.join(tmpdir, sasfilename)
-    columns = read_columns(saspath)
-
-    # Setup the path to the .dat file
-    datfilename = os.path.basename(filename)[:-4] + '.dat'
-    datpath = os.path.join(tmpdir, datfilename)
-
-    # Open the output file
-    outfilename = filename[:-4] + '.csv'
-    badlines = []
-    with open(outfilename, 'wt') as outfile:
-      # Write the column headers
-      writer = csv.writer(outfile)
-      writer.writerow([col['name'] for col in columns])
-      
-      # Read in the data
-      with click.progressbar(length=os.stat(datpath).st_size) as bar:
-        with open(datpath, 'rb') as infile:
-          for line in infile:
-            bar.update(len(line))
-            line = line.decode('latin1')
-
-            # Read in a single row
-            readrow = []
-            for col in columns:
-              val = line[col['start'] - 1:col['end']].strip()
-              
-              # If it's numeric and not missing, format it nicely
-              if val and not col['is_char']:
-                if '.' in val:
-                  val = float(val)
+                if sline[i] == "$":
+                    is_char = True
+                    i += 1
                 else:
-                  val = int(float(val))  # Handle 6e+10
-              readrow.append(val)
+                    is_char = False
 
-            # Write out the row
-            writer.writerow(readrow)
-  if badlines:
-    badfilename = filename[:-4] + '.bad'
-    with open(badfilename, 'wb') as f:
-      for line in badlines:
-        f.write(line)
+                field_range = sline[i]
+                i += 1
+
+                # Field ranges are formatted either as # or #-#
+                sfield_range = field_range.split("-")
+                if len(sfield_range) == 1:
+                    sfield_range = (sfield_range[0], sfield_range[0])
+
+                # Write out the column to the list
+                columns.append(
+                    {
+                        "name": col_name,
+                        "is_char": is_char,
+                        "start": int(sfield_range[0]),
+                        "end": int(sfield_range[1]),
+                    }
+                )
+
+            if last_line:
+                break
+
+    return columns
+
+
+def convert_file(filename: Union[str, Path]):
+    """
+    Convert a file from the Sentencing Commission format into a CSV.
+    Assumes the file is a ZIP file containing at least the following:
+      - .sas: A file with the same name as `filename` except ending in .sas
+      - .dat: A file with the same name as `filename` except ending in .dat
+
+    The .dat file is a fixed-width file whose columns are described by the .sas
+    file. If you're looking at the .sas file, search for INPUT and LENGTH to
+    see the two main parts of the file. There are a _lot_ of columns.
+
+    Args:
+      filename: The name of the file to convert
+    """
+    filename = Path(filename)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir)
+
+        # Unzip the contents of the file
+        with zipfile.ZipFile(filename, "r") as thefile:
+            thefile.extractall(tmpdir)
+
+        # Read in the column names from the .sas file
+        sasfilename = filename.with_suffix(".sas").name
+        saspath = tmpdir / sasfilename
+        columns = read_columns(saspath)
+
+        # Setup the path to the .dat file
+        datfilename = filename.with_suffix(".dat").name
+        datpath = tmpdir / datfilename
+
+        # Open the output file
+        outfilename = filename.with_suffix(".csv")
+        badlines = []
+        with open(outfilename, "wt") as outfile:
+            # Write the column headers
+            writer = csv.writer(outfile)
+            writer.writerow([col["name"] for col in columns])
+
+            # Read in the data
+            with click.progressbar(length=os.stat(datpath).st_size) as bar:
+                with open(datpath, "rb") as infile:
+                    for line in infile:
+                        bar.update(len(line))
+                        line = line.decode("latin1")
+
+                        # Read in a single row
+                        readrow = []
+                        for col in columns:
+                            val = line[col["start"] - 1 : col["end"]].strip()
+
+                            # If it's numeric and not missing, format it nicely
+                            if val and not col["is_char"]:
+                                if "." in val:
+                                    val = float(val)
+                                else:
+                                    val = int(float(val))  # Handle 6e+10
+                            readrow.append(val)
+
+                        # Write out the row
+                        writer.writerow(readrow)
+
+    if badlines:
+        badfilename = filename.with_suffix(".bad")
+        with open(badfilename, "wb") as f:
+            for line in badlines:
+                f.write(line)
 
 
 def main():
-  for filename in sys.argv[1:]:
-    convert_file(filename)
+    for filename in sys.argv[1:]:
+        convert_file(filename)
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
--- getdata.sh
+++ getdata.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget https://www.ussc.gov/sites/default/files/zip/opafy19nid.zip
+wget https://www.ussc.gov/sites/default/files/zip/opafy18nid.zip
 wget https://www.ussc.gov/sites/default/files/zip/opafy17-nid.zip
 wget https://www.ussc.gov/sites/default/files/zip/opafy16-nid.zip
 wget https://www.ussc.gov/sites/default/files/zip/opafy15nid.zip
@@ -11,8 +13,13 @@ wget https://www.ussc.gov/sites/default/files/zip/opafy10nid.zip
 wget https://www.ussc.gov/sites/default/files/zip/opafy09nid.zip
 wget https://www.ussc.gov/sites/default/files/zip/opafy08nid.zip
 
-mv opafy17-nid.zip opafy17nid.zip
-mv opafy16-nid.zip opafy16nid.zip
-
 mkdir -p data
 mv *.zip data
+
+for filename in $(ls data/*.zip); do
+  echo "Working on ${filename}"
+  python3 convert.py "${filename}"
+  echo "Converting to xz"
+  pv "${filename%????}.csv" | xz --stdout - > "${filename%????}.csv.xz"
+  rm "${filename}"
+done
